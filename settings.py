fb43d048dcfa6fdd852f1198452c15a67d31c7e1
marcinskli <marcin.s.klimek@gmail.com>
2021-01-24 19:18:29 +0100

    Version 1.0 of Database filter

---
+++ settings.py
@@ -0,0 +1,13 @@
+# Conditions for database filter
+minMoves = 16
+minElo = 2400
+
+# File names for databases
+database_to_filter = ''
+filtered_database = 'filtered_database.pgn'
+database_low_rated = 'low_rated_database.pgn'
+database_too_short = 'too_short_games.pgn'
+database_incorrect_result = 'incorrect_result.pgn'
+database_chess960 = 'chess960.pgn'
+
+# Generated messages
